65deab0ba67ff1212046ebc0a612becf10dced10
82038198 <82038198@MPB26475.correiosnet.int>
2014-12-18 10:07:37 -0300

    uglify

---
+++ public_html/Gruntfile.js
@@ -0,0 +1,25 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        concat: {
+            dist: {
+                src: [
+                    'js/libs/*.js',
+                    'js/*.js'
+                ],
+                dest: 'js/build/production.js'
+            }
+        },
+        uglify: {
+            build: {
+                src: 'js/build/production.js',
+                dest: 'js/build/production.min.js'
+            }
+        }
+    });
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.registerTask('default', ['concat', 'uglify']);
+};
+
+
